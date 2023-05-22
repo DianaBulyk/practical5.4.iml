90b1ee517b45eafc08adca2f4e554bcf171006f4
wodxsc <dinka5644@gmail.com>
2023-05-22 13:06:36 +0300

    Initial commit

---
+++ src/task1.java
@@ -0,0 +1,6 @@
+public class task1 {
+    public static void main(String[] args) {
+        System.out.println("Батько: Чуманенко Степан Дмитрович, 1965 року народження, підприємець");
+        System.out.println("Сестра: Чуманенко Надія Степанівна, 1998 року народження, студентка");
+    }
+}
